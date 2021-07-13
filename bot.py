b0c08d35bd5c9fbdb5af214196b72114aebc1be3
sharry008 <57253595+sharry008@users.noreply.github.com>
2021-07-13 09:03:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,19 +19,19 @@ def action(msg):
         telegram_bot.sendMessage(chat_id, str('1 Python101 book --(/py101)'))  # (/py101 command to acess the file)
         telegram_bot.sendMessage(chat_id, str('2 Python202 book --(/py202)'))  # (/py202 command to acess the file)
         telegram_bot.sendMessage(chat_id, str('3 Basic Networking notes --(/netw)'))
-        telegram_bot.sendMessage(chat_id, str('4 C++ Notes --(/c1)'))
+        telegram_bot.sendMessage(chat_id, str('4 MPR FILE --(/c1)'))
     elif command == '/time':
         telegram_bot.sendMessage(chat_id, str(now.hour) + str(":") + str(now.minute))
     elif command == '/logo':
         telegram_bot.sendPhoto(chat_id, photo=" https://photos.app.goo.gl/8Z7ivRDSA4bJpCmc7")
     elif command == '/py101':
-        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/py101'))
+        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/py101.pdf'))
     elif command == '/py202':
-        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/py202'))
+        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/py202.pdf'))
     elif command == '/netw':
-        telegram_bot.sendDocument(chat_id, document=open('/C:/Users/sharm/Documents/bot/netw'))
+        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/netw.pdf'))
     elif command == '/c1':
-        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/c1'))
+        telegram_bot.sendDocument(chat_id, document=open('/home/pi/Documents/c1.pdf'))
 
 
 telegram_bot = telepot.Bot('1817933623:AAHA2ZXYkdotuxPtHLBYhCfepxKELRPOhzw')
